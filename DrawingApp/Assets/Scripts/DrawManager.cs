91d777c15d977c14c2378f038daabc8d79dc9096
Ross CH <ross.harvey71@gmail.com>
2020-04-26 21:22:39 +0100

    implemented free drawing tool

---
+++ DrawingApp/Assets/Scripts/DrawManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawManager: MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public GameObject drawingPrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject drawing = Instantiate(drawingPrefab);
+            lineRenderer = drawing.GetComponent<LineRenderer>();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            FreeDraw();
+        }
+    }
+
+    void FreeDraw()
+    {
+        lineRenderer.startWidth = 0.06f;
+        lineRenderer.endWidth = 0.06f;
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, Camera.main.ScreenToWorldPoint(mousePos));
+
+    }
+}
